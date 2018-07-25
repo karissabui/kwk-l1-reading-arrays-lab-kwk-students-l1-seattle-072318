f4a4646ab4ab14449f40235a44aeb72a0acfe6ec
Karissa Bui <karissab002@gmail.com>
2018-07-25 17:18:56 +0000

    automatically backed up by learn

--- seattle_arrays.rb
+++ seattle_arrays.rb
@@ -1,7 +1,7 @@
 cart_items_prices = [1.99, 5.76. 13.45, 12.99]
 
 count = 0 
-car_items_prices.each do |price|
+cart_items_prices.each do |price|
   puts "Item #{count}: #{price}"
   count += 1 
 end
