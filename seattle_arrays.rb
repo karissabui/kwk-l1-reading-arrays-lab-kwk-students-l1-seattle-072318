95bdde5719ff20511e0725fdaa1b90b5f4705875
Karissa Bui <karissab002@gmail.com>
2018-07-25 17:26:13 +0000

    automatically backed up by learn

--- seattle_arrays.rb
+++ seattle_arrays.rb
@@ -1,7 +1,17 @@
-cart_items_prices = [1.99, 5.76, 13.45, 12.99]
+#cart_items_prices = [1.99, 5.76, 13.45, 12.99]
+
+#count = 0 
+#cart_items_prices.each do |price|
+  #puts "Item #{count}: #{price}"
+  #count += 1 
+#end
+
+
+
+expensive_items = [1.99, 5.79, 13.45, 12.99]
 
 count = 0 
-cart_items_prices.each do |price|
+expensive_items.each do |price|
   puts "Item #{count}: #{price}"
-  count += 1 
+  count += 1
 end
