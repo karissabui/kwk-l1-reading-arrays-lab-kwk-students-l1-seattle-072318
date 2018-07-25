b0c40a249e7431182c264d67f1ce42273aeb6984
Karissa Bui <karissab002@gmail.com>
2018-07-25 17:18:11 +0000

    automatically backed up by learn

--- seattle_arrays.rb
+++ seattle_arrays.rb
@@ -1,5 +1,7 @@
-array = ["puppies", "kittens"]
+cart_items_prices = [1.99, 5.76. 13.45, 12.99]
 
-array.each do |pets|
-  puts "#{pets}"
+count = 0 
+car_items_prices.each do |price|
+  puts "Item #{count}: #{price}"
+  counter += 1 
 end
